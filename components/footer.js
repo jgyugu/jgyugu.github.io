0d9485c6d5d4a16d5cdd17d300805ee44f2367c0
jgyugu <katt_2021@qq.com>
2026-02-14 21:07:00 +0800

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,9 @@
 document.write(`
 <div class="footer">
   <div class="footer-links">
-    <a href="#">关于维基</a>
-    <a href="#">维基规则</a>
-    <a href="#">更新日志</a>
+    <a href="../qun.html">关于维基</a>
+    <a href="../qun.html">维基规则</a>
+    <a href="../qun.html">更新日志</a>
     <a href="../qun.html">联系我们</a>
   </div>
   <div class="copyright">© 2026 rooms of loop维基</div>
